dcb2ce87a21024226d02419c2cf0f3308ca1d954
Akshayt12 <at16938@gmail.com>
2021-10-28 09:32:04 +0530

    Commit

--- entry/urls.py
+++ entry/urls.py
@@ -1,10 +1,16 @@
 """entry URL Configuration"""
 from django.contrib import admin
 from django.urls import path,include
+
+from entry import settings
 from . import views
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('',views.index,name='index'),
     path('director/',include('director.urls')),
     path('gaurd/',include('gaurd.urls')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
